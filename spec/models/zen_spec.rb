fd16e7e697d9f6eb2ed090ea13cf10fe2a350085
kter <me@kter.jp>
2018-11-23 14:54:23 +0900

    テスト追加

--- spec/models/zen_spec.rb
+++ spec/models/zen_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
+# describe, context, before, afterを使ってDRYにする
 
 RSpec.describe Zen, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without content" do
+    zen = Zen.new(
+      content: nil,
+      deleted: false
+    )
+    zen.valid?
+    expect(zen.errors[:content]).to include("can't be blank")
+  end
+
+  it "is invalid without deleted flag" do
+    zen = Zen.new(
+      content: "test content 3",
+      deleted: nil
+    )
+    zen.valid?
+    expect(zen).to be_invalid
+  end
+
+  it "returns a content as a string" do
+    zen = Zen.new(
+      content: "test content 1",
+      deleted: false
+    )
+    expect(zen.content).to be_a_kind_of(String)
+  end
+
+  it "is valid with content and true deleted flag" do
+    zen = Zen.new(
+      content: "test content 1",
+      deleted: true
+    )
+    expect(zen).to be_valid
+  end
+
+  it "is valid with content and false deleted flag" do
+    zen = Zen.new(
+      content: "test content 2",
+      deleted: false
+    )
+    expect(zen).to be_valid
+  end
+
+  it "is invalid with 250 over content" do
+    zen = Zen.new(
+      content: "あ" * 251,
+      deleted: false
+    )
+    expect(zen).to be_invalid
+  end
 end
