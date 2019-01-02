4a8ea3b8097bbc64919f78f70827ed6ba224da24
kter <me@kter.jp>
2019-01-03 02:13:55 +0900

    コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'auth/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :zens
   root "zens#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
