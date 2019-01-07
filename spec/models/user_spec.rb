28a86431aad77f8d3fb8adcebe6dc0879f9e97ed
kter <me@kter.jp>
2019-01-08 08:36:14 +0900

    よくわからず。とりあえずコミット

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :user
+  # login_user
+
+  let(:valid_attrigutes) {}
+  let(:valid_session) {}
+
+  describe "GET show" do
+    it "assigns the requested user as @user" do
+      sign_in user(:takahashi)
+      get :show, {id: user.to_param}, valid_session
+      expect(assigns :story).to eq user
+   end
+  end
+
+  it "returns name if nickname does not exist" do
+    sign_in user(:sato)
+    expect(current_user.get_nickname).to include("Sato")
+  end
+
+  it "returns nickname if nickname does exist" do
+    sign_in user(:takahashi)
+    expect(current_user.get_nickname).to include("taka-chan")
+  end
 end
