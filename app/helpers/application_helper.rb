3a03092171be4a7b49511e7b13d1de15c67a1a25
kter <me@kter.jp>
2019-01-17 00:40:29 +0900

    ユーザーページを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     def app_name
         ENV['APP_NAME'].dup.force_encoding('UTF-8') || ""
     end
+
+    def logged_in?
+        current_user ? true : false
+    end
 end
