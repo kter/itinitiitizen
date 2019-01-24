713c6b35aa1a05ea0441e619eb8c070713852d7a
kter <me@kter.jp>
2019-01-25 01:10:50 +0900

    ユーザー登録時にエラーが出ないようemailを非必須にする

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_001707) do
+ActiveRecord::Schema.define(version: 2019_01_24_160556) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
