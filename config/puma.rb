1f54f679d415147552d53d0357d4f1139f53c483
kter <me@kter.jp>
2019-01-26 18:05:57 +0900

    Pumaの設定を追加

--- config/puma.rb
+++ config/puma.rb
@@ -28,7 +28,15 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_worker_boot do
+    # Worker specific setup for Rails 4.1+
+    # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+    ActiveRecord::Base.establish_connection
+end
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+rackup      DefaultRackup
