62b786e87eeba0e1294b0f5d3a4385045f8680ac
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-07-07 17:41:19 +0900

    undo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_032501) do
+ActiveRecord::Schema.define(version: 2019_06_22_075458) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,8 +61,6 @@ ActiveRecord::Schema.define(version: 2019_07_07_032501) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "report_count", default: 0, null: false
-    t.boolean "spam", default: false, null: false
     t.index ["user_id"], name: "index_zens_on_user_id"
   end
 
