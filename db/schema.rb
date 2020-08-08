d25b97068e5280f26e111610437c5d7733d7123c
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-08-08 18:03:01 +0000

    何故かuidとtokenカラムが追加できなかったので後から追加するようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_003743) do
+ActiveRecord::Schema.define(version: 2020_08_08_173822) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,15 +33,6 @@ ActiveRecord::Schema.define(version: 2020_07_30_003743) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "reports", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "zens_id"
-    t.integer "report_count", default: 0, null: false
-    t.boolean "spam", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["zens_id"], name: "index_reports_on_zens_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
@@ -55,11 +46,11 @@ ActiveRecord::Schema.define(version: 2020_07_30_003743) do
     t.string "last_sign_in_ip"
     t.string "users"
     t.string "provider"
-    t.string "uid"
-    t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "uid"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -74,6 +65,5 @@ ActiveRecord::Schema.define(version: 2020_07_30_003743) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "reports", "zens", column: "zens_id"
   add_foreign_key "zens", "users"
 end
